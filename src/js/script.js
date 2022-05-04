8f571517114beab9576e56394b42b161a27cfa2b
Kadeshnik <victor.zhovtyi.k@gmail.com>
2022-05-04 20:06:46 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert(5);
+
+
+// const result = confirm("are you here");
+
+// console.log(result);
+
+// const answer = prompt("How old are you", "18");
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя', '');
+answers[1] = prompt('Как ваше фамилия', '');
+answers[2] = prompt('Как ваше лет', '');
